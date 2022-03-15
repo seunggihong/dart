f6e3d64dad6143af182d14be4cf7c2361789fcd7
onggi <hsk1533@naver.com>
2022-03-15 17:50:33 +0900

    function

---
+++ lib/func.dart
@@ -0,0 +1,11 @@
+// function
+void printInteger(int number) {
+  print("The number is ${number}.");
+}
+
+void main() {
+  var number = 32;
+  // 'var' keyword : Data type is not specified.
+
+  printInteger(number);
+}
